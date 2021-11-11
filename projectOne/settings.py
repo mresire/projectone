01352628589b9ca680f86151cd674f7c6ec7fab0
mresire <mresire@gmail.com>
2021-11-11 14:29:17 +0200

    added whitenoise in settings

--- projectOne/settings.py
+++ projectOne/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +136,8 @@ USE_TZ = True
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
@@ -142,5 +145,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # django_heroku.settings(locals())
