eb527dfd7175fd1b9c31860e069332767d907d8c
mresire <mresire@gmail.com>
2021-11-17 16:56:50 +0200

    added api app with documentation

--- projectOne/urls.py
+++ projectOne/urls.py
@@ -19,10 +19,34 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Task API",
+      default_version='v1.1',
+      description="An api for managing tasks",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@contacts.remote"),
+      license=openapi.License(name="Lorem ipsum License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+   authentication_classes=[]
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Product.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
+    path('api/',include('api.urls')),
+
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('documentation', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
